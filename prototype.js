2ff97556595a5d4998c10e8150c6519757c82803
nbappi <bappi.cse562@gmail.com>
2016-09-04 15:01:14 +0600

    Second prototype example

--- prototype.js
+++ prototype.js
@@ -13,4 +13,45 @@ console.log(personObj);     // return own object
 console.log(Object.getPrototypeOf(personObj));   // return a greeting object
 console.log(personObj.hasOwnProperty('canCheck'));   // true
 console.log(personObj.hasOwnProperty('greeting'));  // false
-personObj.greeting();
+personObj.greeting();  //  Hi I am Bappi
+
+/* ...........Second prototype................*/ 
+console.log("-----------------------------------------------------------------");
+
+var body = function(){
+	this.check = true;
+};
+
+body.prototype.info = function(){
+    if(this.check){
+       console.log("Hi I am "+ this.name);
+    }
+};
+
+var employee = function(name, title){
+	body.call(this);
+	this.name = name;
+	this.title = title;
+}
+
+employee.prototype = Object.create(body.prototype);
+
+var employeeObj = new employee('Nuruzzaman', "Software Engineer");
+employeeObj.info();
+
+console.log(Object.getPrototypeOf(employeeObj));  // Object
+console.log(employeeObj.hasOwnProperty('info'));  // false
+
+console.log("---------------------------------------------------------");
+
+var customer = function(name , check ){
+	body.call(this);
+	this.name = name;
+	this.check = check;
+}; 
+
+customer.prototype = Object.create(body.prototype);
+var customerObj = new customer('Rahim customer', true );  // pass true or false ;;;;;  false = No data display, true = data display. 
+customerObj.info();
+
+
